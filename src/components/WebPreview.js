a4e27e4c332a607f35595d6d6e61f426ef7b0ee5
Pmc Dev <phoomparin@gmail.com>
2018-04-08 05:10:59 +0700

    :sparkles: Render WebPreview in Iframe

--- src/components/WebPreview.js
+++ src/components/WebPreview.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'
+import styled from 'react-emotion'
 
 import Button from '../components/Button'
 
+const Frame = styled.iframe`
+  display: flex;
+
+  width: 60vw;
+  height: 100vh;
+
+  border: none;
+`
+
 export default class WebPreview extends Component {
   state = {open: false}
+
+  componentDidUpdate() {
+    const {src} = this.props
+    const ref = this.iframe
+
+    if (ref) {
+      const iframe =
+        ref.contentWindow ||
+        (ref.contentDocument.document || ref.contentDocument)
+
+      iframe.document.open()
+      iframe.document.write(src)
+      iframe.document.close()
+    }
+  }
 
   toggle = () => this.setState({open: !this.state.open})
 
   render() {
     const {open} = this.state
-    const {src} = this.props
 
     return (
       <div>
@@ -17,13 +41,7 @@ export default class WebPreview extends Component {
           ดูผลลัพธ์ของโค้ด HTML
         </Button>
 
-        {open && (
-          <div
-            dangerouslySetInnerHTML={{
-              __html: src,
-            }}
-          />
-        )}
+        {open && <Frame innerRef={r => (this.iframe = r)} />}
       </div>
     )
   }
