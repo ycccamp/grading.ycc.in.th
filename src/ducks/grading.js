36288ee016ecd9459cd70b10af998e8af353418f
Pmc Dev <phoomparin@gmail.com>
2018-04-07 15:50:55 +0700

    :sparkles: Added Max Scores

--- src/ducks/grading.js
+++ src/ducks/grading.js
@@ -1,24 +1,54 @@
 import R from 'ramda'
 
-// camper1: {
-//   general: [{
-//     gradedBy: 'tul',
-//     scores: [7, 10, 8],
-//     notes: '-'
-//   }],
-//   major: []
-// }
+const sample = {
+  core: [
+    {
+      gradedBy: 'A',
+      scores: [7, 10, 8],
+      notes: 'Good',
+    },
+    {
+      gradedBy: 'B',
+      scores: [10, 9, 15],
+    },
+  ],
+  major: [
+    {
+      gradedBy: 'C',
+      scores: [10, 10, 10],
+    },
+  ],
+}
 
-function average(results) {
+const maxScores = {
+  core: [10, 15, 15],
+  design: [20, 20, 20],
+  content: [45, 15],
+  marketing: [20, 20, 20],
+  programming: [20, 20, 20],
+}
+
+function assertMax(section, scores) {
+  const max = maxScores[section]
+
+  for (const i in max) {
+    if (scores[i] > max[i]) {
+      throw new Error('given score exceeds maximum score')
+    }
+  }
+}
+
+function average(results, section = 'core') {
   // TODO: Filter out unfinished graders
   const scores = results.map(result => R.sum(result.scores))
+  assertMax(section, scores)
 
   return R.sum(scores) / scores.length
 }
 
-function finalScore(camper) {
-  const general = average(camper.general)
-  const major = average(camper.major)
+function finalScore(grade, camper) {
+  const core = average(grade.core)
+  const major = average(grade.major, camper.major)
 
-  return general + major
+  return core + major
 }
