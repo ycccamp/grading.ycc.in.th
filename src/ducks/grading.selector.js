44a1b9ddb5610b87d94684fcc932db4655e2c47c
Pmc Dev <phoomparin@gmail.com>
2018-04-16 03:38:11 +0700

    :bug: Fixed Incorrect Remaining Number in Submissions

--- src/ducks/grading.selector.js
+++ src/ducks/grading.selector.js
@@ -4,59 +4,6 @@ import {createSelector} from 'reselect'
 import {getEvaluation} from '../core/evaluation'
 
 /*
-  # Counting Selectors
-    These selectors will be used for counting submissions in routes/submissions
-*/
-
-// Calculate the total and delisted submissions
-export const totalSelector = createSelector(
-  s => s.camper.campers,
-  s => s.grading.data,
-  (campers, entries) => {
-    const delisted = entries.filter(x => x.delisted).length
-
-    return {total: campers.length - delisted, delisted}
-  },
-)
-
-// Show how many people have been graded
-export const gradedSelector = createSelector(
-  s => s.grading.data,
-  s => s.user.role,
-  s => s.user.name,
-  (entries, role, name) => {
-    const type = role === 'core' ? 'core' : 'major'
-
-    return entries.filter(x => !x.delisted && x[type] && x[type][name]).length
-  },
-)
-
-/*
-  # Evaluations Selector
-*/
-
-export const evaluationsSelector = createSelector(
-  s => s.camper.campers,
-  s => s.grading.data,
-  s => s.user.name,
-  s => s.user.role,
-  (campers, entries, gradedBy, role) => {
-    const submissions = campers.map(camper => {
-      const evaluations = entries.find(entry => entry.id === camper.id)
-
-      const evaluation = getEvaluation(evaluations, gradedBy, role)
-
-      return {
-        id: camper.id,
-        ...evaluation,
-      }
-    })
-
-    return submissions.filter(x => !x.delisted)
-  },
-)
-
-/*
   # Submissions Record Selectors
     This selectors will be used to populate the data in SubmissionsRecord
 */
@@ -97,6 +44,55 @@ export const submissionsSelector = createSelector(
     })
 
     return submissions.sort(sortByGraded).map(withIndex)
+  },
+)
+
+/*
+  # Counting Selectors
+    These selectors will be used for counting submissions in routes/submissions
+*/
+
+// Calculate the total and delisted submissions
+export const totalSelector = createSelector(submissionsSelector, entries => {
+  const delisted = entries.filter(x => x.delisted).length
+
+  return {total: entries.length - delisted, delisted}
+})
+
+// Show how many people have been graded
+export const gradedSelector = createSelector(
+  s => s.grading.data,
+  s => s.user.role,
+  s => s.user.name,
+  (entries, role, name) => {
+    const type = role === 'core' ? 'core' : 'major'
+
+    return entries.filter(x => !x.delisted && x[type] && x[type][name]).length
+  },
+)
+
+/*
+  # Evaluations Selector
+*/
+
+export const evaluationsSelector = createSelector(
+  s => s.camper.campers,
+  s => s.grading.data,
+  s => s.user.name,
+  s => s.user.role,
+  (campers, entries, gradedBy, role) => {
+    const submissions = campers.map(camper => {
+      const evaluations = entries.find(entry => entry.id === camper.id)
+
+      const evaluation = getEvaluation(evaluations, gradedBy, role)
+
+      return {
+        id: camper.id,
+        ...evaluation,
+      }
+    })
+
+    return submissions.filter(x => !x.delisted)
   },
 )
 
