e113638b38df3ff57b7cd7ca0aae6e67bcfa6798
pontakornth <pontakorn61@gmail.com>
2020-03-05 09:18:25 +0700

    Use new firebase config

--- src/core/fire.js
+++ src/core/fire.js
@@ -3,6 +3,18 @@ import '@firebase/firestore'
 import SagaFirebase from 'redux-saga-firebase'
 
 const config = {
+  apiKey: 'AIzaSyBoT3SIWbgGNf7QEqMIL8p3mHUWe7y-HuI',
+  authDomain: 'ycc2020.firebaseapp.com',
+  databaseURL: 'https://ycc2020.firebaseio.com',
+  projectId: 'ycc2020',
+  storageBucket: 'ycc2020.appspot.com',
+  messagingSenderId: '959291668430',
+  appId: '1:959291668430:web:ab7ec20b9a3cb0a6879d9f',
+  measurementId: 'G-6KCXHLX74P',
+}
+
+/*
+const Oldconfig = {
   apiKey: 'AIzaSyD69fVmX1N539fYPjj4X2mu7hDR4LYAnL8',
   authDomain: 'ycccamp.firebaseapp.com',
   databaseURL: 'https://ycccamp.firebaseio.com',
@@ -10,6 +22,7 @@ const config = {
   storageBucket: 'ycccamp.appspot.com',
   messagingSenderId: '191460697180',
 }
+*/
 
 export const app = firebase.initializeApp(config)
 
