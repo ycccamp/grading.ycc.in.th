89fa26ae83d2f3274351a630d801faa7b52fea37
pontakornth <pontakorn61@gmail.com>
2020-03-08 08:52:35 +0700

    Revert to username and password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Form, Icon} from 'antd'
+import {Field, reduxForm} from 'redux-form'
+import {TextField} from 'redux-form-antd'
+
+import Label from '../components/Label'
 import Button from '../components/Button'
 
-/* const Input = ({icon, ...props}) => (
+const Input = ({icon, ...props}) => (
   <Field
     component={TextField}
     size="large"
@@ -11,18 +16,29 @@ import Button from '../components/Button'
   />
 )
 
-*/
-
 const LoginForm = ({handleSubmit}) => (
-  <div>
+  <Form onSubmit={handleSubmit}>
+    <div>
+      <Label>ชื่อผู้ใช้:</Label>
+      <Input name="username" icon="user" placeholder="Username" />
+    </div>
+    <div style={{marginTop: '1.5em'}}>
+      <Label>รหัสผ่าน:</Label>
+      <Input
+        name="password"
+        icon="lock"
+        placeholder="Password"
+        type="password"
+      />
+    </div>
     <div style={{marginTop: '1.5em', alignSelf: 'flex-end'}}>
       <Button type="primary" htmlType="submit" size="large" icon="login">
-        เข้าสู่ระบบด้วย Google
+        เข้าสู่ระบบ
       </Button>
     </div>
-  </div>
+  </Form>
 )
-/*
+
 const validate = values => {
   const errors = {}
 
@@ -36,8 +52,7 @@ const validate = values => {
 
   return errors
 }
-*/
 
-// const enhance = reduxForm({form: 'login', validate})
+const enhance = reduxForm({form: 'login', validate})
 
-export default LoginForm
+export default enhance(LoginForm)
